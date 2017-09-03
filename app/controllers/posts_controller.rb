962d14d6a8dbb7186537302d6edfaa13b96640c5
Tim Provencio <tim@provencio.com>
2017-09-03 12:10:57 -0700

    Doing a little DRYing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@posts = Post.all
 	end
@@ -10,30 +12,46 @@ class PostsController < ApplicationController
 	end
 
         def create
-            @post = Post.create(post_params)
-            redirect_to posts_path
+
+	    if @post = Post.create(post_params)
+
+		flash[:success] = "Your post has been created!"
+ 		redirect_to posts_path
+    	    else
+		flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      		render :new
+    	    end
+
         end
+
 
     	private def post_params
            params.require(:post).permit(:image, :caption)
         end
 
+	def set_post
+    	    @post = Post.find(params[:id])
+  	end
+	
 	def show
-	    @post = Post.find(params[:id])
+	
 	end
 
 	def edit
-	    @post = Post.find(params[:id])
+
 	end
 
 	def update
-	    @post = Post.find(params[:id])
-	    @post.update(post_params)
-	    redirect_to(post_path(@post))
+	    if @post.update(post_params)
+		flash[:success] = "Post updated."
+      		redirect_to posts_path
+    	    else
+		flash.now[:alert] = "Update failed.  Please check the form."
+      		render :edit
+    	    end	
 	end
 	
 	def destroy
-	    @post = Post.find(params[:id])
 	    @post.destroy
 	    redirect_to posts_path
 	end
