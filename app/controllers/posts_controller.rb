72e9d96f237e8186555290c2e47e01962c4c6129
Tim Provencio <tim@provencio.com>
2017-09-03 11:52:53 -0700

    Adds show and edit but needs to be DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,29 @@ class PostsController < ApplicationController
             redirect_to posts_path
         end
 
-    private
-
-        def post_params
+    	private def post_params
            params.require(:post).permit(:image, :caption)
         end
 
+	def show
+	    @post = Post.find(params[:id])
+	end
+
+	def edit
+	    @post = Post.find(params[:id])
+	end
+
+	def update
+	    @post = Post.find(params[:id])
+	    @post.update(post_params)
+	    redirect_to(post_path(@post))
+	end
+	
+	def destroy
+	    @post = Post.find(params[:id])
+	    @post.destroy
+	    redirect_to posts_path
+	end
 end
+
+
