e4ebd2298a634cce119e0b70536eb6909109c0d1
Tim Provencio <tim@provencio.com>
2017-09-03 18:20:27 -0700

    Adds base Comment code

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :comments, dependent: :destroy
+
   validates :image, presence: true
   validates :caption, length: { minimum: 3, maximum: 300 }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
 
